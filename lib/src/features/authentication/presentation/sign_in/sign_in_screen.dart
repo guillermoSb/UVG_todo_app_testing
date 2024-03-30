d1147ecb7d20aba13a0507e7315842517a425cff
Guillermo Santos <guillermo.santosba@gmail.com>
2024-03-30 12:56:50 -0600

    Added content rights

--- lib/src/features/authentication/presentation/sign_in/sign_in_screen.dart
+++ lib/src/features/authentication/presentation/sign_in/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/src/constants/app_sizes.dart';
 
 /// User Sign In Screen.
 class SignInScreen extends StatelessWidget {
@@ -6,6 +7,49 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: const AuthenticationForm(),
+    );
+  }
+}
+
+/// Email & Password Authentication Form. Supports:
+/// - sign in
+/// - sign up
+class AuthenticationForm extends StatelessWidget {
+  const AuthenticationForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: Sizes.p16),
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              autocorrect: false,
+            ),
+            gapH12,
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Password'),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              obscureText: true,
+              autocorrect: false,
+            ),
+            gapH12,
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Sign In'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
