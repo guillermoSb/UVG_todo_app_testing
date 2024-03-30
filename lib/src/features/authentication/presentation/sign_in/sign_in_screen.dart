8c7309526976fc26cf915ce51ac36f8cb14072be
Guillermo Santos <guillermo.santosba@gmail.com>
2024-03-30 11:37:01 -0600

    Created Sign In screen.

---
+++ lib/src/features/authentication/presentation/sign_in/sign_in_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// User Sign In Screen.
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
