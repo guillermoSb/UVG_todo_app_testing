9b0fe61a72f5d12ce40950f7a016271a041b2335
Guillermo Santos <guillermo.santosba@gmail.com>
2024-04-01 19:08:14 -0600

    feature/todo

---
+++ test/src/features/todo/presentation/todo_list/todo_list_screen_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Todo List Screen', () {
+    testWidgets('''
+    Given - User has 3 tasks
+    When - Screen appears
+    Then - 3 tasks are shown
+    ''', (tester) async {
+      // Arrange
+      // Act
+      // Assert
+    });
+  });
+}
